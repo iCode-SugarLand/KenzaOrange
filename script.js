840e0952b899e479b435ab8afec5255253dbd76a
iCode-SugarLand <124052535+iCode-SugarLand@users.noreply.github.com>
2024-05-15 19:38:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const water = new Audio("waterdrop.mp3");
+
+window.addEventListener("mousemove",()=>{
+  water.play();
+});
+const sisters = new Audio("sisterstalk.mp3");
+window.addEventListener("click",()=>{
+  sisters.play();
+});
